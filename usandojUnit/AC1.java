42cc9bea3de3e0f32c8cf2bd3af160221f89e35e
grupofitgkpr <78859949+grupofitgkpr@users.noreply.github.com>
2021-02-16 13:53:53 -0300

    Add files via upload
    Acho que é isso :v

---
+++ usandojUnit/AC1.java
@@ -0,0 +1,22 @@
+package usandojUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AC1 {
+
+	@Test
+	void test() {
+		Arranjo a = new Arranjo();
+		
+		assertEquals(510, a.menor(), "O menor deve ser 510");
+		assertEquals(940, a.maior(), "O maior é 940");
+		assertEquals(7540, a.soma(), "A soma é 7040");
+		assertEquals(0, a.repeticoes(3), "3 não aparece");
+		assertEquals(1, a.repeticoes(790), "790 aparece 1 vez");
+		assertEquals(2, a.repeticoes(940), "940 aparece 2 vezes");
+		
+	}
+
+}
